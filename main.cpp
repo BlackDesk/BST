8ed9518da73e3beaf6ff971a112b4c3dd5063de3
Vasily Vyalkov <vv@aircoud.org>
2020-05-05 22:47:01 +0300

    Finish separating definition & declaration

--- main.cpp
+++ main.cpp
@@ -14,88 +14,47 @@ public:
     using size_type = std::size_t;
     using node = Node;
 
-    // constructors & destructor
+
+    // CONSTRUCTORS & DESTRUCTOR
+
     BST() : root(nullptr) {};
 
-    BST(Node *_root) : root(_root) {};
+    explicit BST(Node *_root) : root(_root) {};
 
     ~BST() { clear(); };
 
-    // member functions
 
-    void clear();           // erases all nodes
+    // MEMBER FUNCTIONS
+
+    void clear();
+    // erases all nodes O(n)
+
     value_type size() const;
+    // nodes amount O(1)
 
+    value_type min() const;
+    // min key O(h)
 
-    value_type min() const {
-        Node *curr = root;
-        while (curr->left) {
-            curr = curr->left;
-        }
-        return curr->key;
-    }
+    value_type max() const;
+    // max key O(h)
 
-    value_type max() const {
-        Node *curr = root;
-        while (curr->right) {
-            curr = curr->right;
-        }
-        return curr->key;
-    }
+    value_type *find(const value_type &key) const;
+    // pointer on key if is exists or nullptr
 
-    value_type *find(const value_type &key) const {
-        return &find_node(root, key)->key;
-    } // nullptr if no value found
+    [[nodiscard]] bool search(const value_type &key) const;
+    // key existing flag
 
-    bool search(const value_type &key) const {
-        find(key) != nullptr;
-    }
+    [[nodiscard]] value_type *next(const value_type &key) const;
+    // next value in no-decreasing order
 
-    value_type *next(const value_type &key) const {
-        Node *node = find_node(root, key);
-        if (node == nullptr) {
-            std::stringstream error;
-            error << "No value " << key << " in tree to find next!\n";
-            throw std::runtime_error(error.str());
-        }
-        auto result_node = next_node(node);
-        return (result_node ? &(result_node->key) : nullptr);
-    } // next value by no-decreasing order, nullptr if no value found;
+    void insert(const value_type &key);
+    // inserts node with key into BST
 
-    void insert(const value_type &key) {
-        Node *node = root;
-        Node *key_node = new Node{key};
-        while (node) {
-            key_node->parent = node;
-            if (node->key < key) {
-                node = node->right;
-            } else {
-                node = node->left;
-            }
-        }
-        if (key_node->parent) {
-            if (key_node->parent->key < key_node->key) {
-                key_node->parent->right = key_node;
-            } else {
-                key_node->parent->left = key_node;
-            }
-        } else {
-            this->root = key_node;
-        }
-        ++this->_size;
-    }
+    void erase(const value_type &key);
+    // erases node with key from BST
 
-    void erase(const value_type &key) {
-        Node *node = find_node(root, key);
-        erase_node(node);
-    }
-
-
-    void get_ordered(std::vector<value_type> &data) const {
-        data.clear();
-        data.reserve(_size);
-        get_ordered(data, root);
-    }
+    void get_ordered(std::vector<value_type> &data) const;
+    // fill array with keys in no-decreasing order
 
 
 private:
@@ -112,113 +71,20 @@ private:
     size_type _size{0};
     Node *root{nullptr};
 
-    Node *find_node(Node *curr_node, const value_type &key) const {
-        if (!curr_node) {
-            return nullptr;
-        }
-        if (curr_node->key > key) {
-            return find_node(curr_node->left, key);
-        }
-        if (curr_node->key < key) {
-            return find_node(curr_node->right, key);
-        }
-        return curr_node;
-    }
+    Node *find_node(Node *curr_node, const value_type &key) const;
+    // get node by key
 
-    Node *next_node(const Node *node) const {
-        Node *curr_node = find_node(root, node->key);
-        if (curr_node->right) {
-            curr_node = curr_node->right;
-            while (curr_node->left) {
-                curr_node = curr_node->left;
-            }
-            return curr_node;
-        }
-        while (curr_node->parent && curr_node->parent->right == curr_node) {
-            curr_node = curr_node->parent;
-        }
-        return (curr_node->parent ? curr_node->parent : nullptr);
-    } // next node with value equal or bigger than in current node
+    Node *next_node(const Node *node) const;
+    // next node with value equal or bigger than in current node
 
-    void get_ordered(std::vector<value_type> &data, Node *node) const {
-        if (node) {
-            get_ordered(data, node->left);
-            data.push_back(node->key);
-            get_ordered(data, node->right);
-        }
-    } // fill vector with sorted elements
+    void get_ordered(std::vector<value_type> &data, Node *node) const;
+    // fill vector with sorted elements
 
-    void erase_node(Node *node) {
-        if (!node || !root) {
-            return;
-        }
+    void erase_node(Node *node);
+    // erases node from BST
 
-
-        if (node == root) {
-            if (!node->left && !node->right) {
-                delete node;
-                root = nullptr;
-                --_size;
-                return;
-            }
-
-            if (node->left && !node->right) {
-                root->key = root->left->key;
-                erase_node(root->left);
-                return;
-            }
-
-            if (!node->left && node->right) {
-                root->key = root->right->key;
-                erase_node(root->right);
-                return;
-            }
-
-            Node *node_next = next_node(root);
-            root->key = node_next->key;
-            erase_node(node_next);
-            return;
-        }
-
-        if (!node->left && !node->right) { // no children
-            (node->parent->left == node ? node->parent->left : node->parent->right) = nullptr;
-            delete node;
-            --_size;
-            return;
-        }
-
-        if (node->right) { // only right child
-            (node->parent->left == node ? node->parent->left : node->parent->right) = node->right;
-            node->right->parent = node->parent;
-            delete node;
-            --_size;
-            return;
-        }
-
-        if (node->left) { // only left child
-            (node->parent->left == node ? node->parent->left : node->parent->right) = node->left;
-            node->left->parent = node->parent;
-            delete node;
-            --_size;
-            return;
-        }
-
-        Node *node_next = next_node(node);
-        if (node_next) {
-            node->key = node_next->key;
-            erase_node(node_next);
-        }
-
-    }
-
-    void erase_subtree(Node *node) {
-        if (node) {
-            erase_subtree(node->left);
-            erase_subtree(node->right);
-            erase_node(node);
-        }
-    }
-
+    void erase_subtree(Node *node);
+    // erases subtree with particular root
 
 };
 
@@ -228,9 +94,196 @@ void BST<T>::clear() {
 }
 
 template<typename T>
-typename BST<T>::value_type BST<T>::size() const {
+T BST<T>::size() const {
     return _size;
-} // nodes cnt
+}
+
+template<typename T>
+T BST<T>::min() const {
+    Node *curr = root;
+    while (curr->left) {
+        curr = curr->left;
+    }
+    return curr->key;
+}
+
+template<typename T>
+T BST<T>::max() const {
+    Node *curr = root;
+    while (curr->right) {
+        curr = curr->right;
+    }
+    return curr->key;
+}
+
+template<typename T>
+T *BST<T>::find(const value_type &key) const {
+    return nullptr;
+}
+
+template<typename T>
+bool BST<T>::search(const value_type &key) const {
+    return find(key) != nullptr;
+}
+
+template<typename T>
+T *BST<T>::next(const value_type &key) const {
+    Node *node = find_node(root, key);
+    if (node == nullptr) {
+        std::stringstream error;
+        error << "No value " << key << " in tree to find next!\n";
+        throw std::runtime_error(error.str());
+    }
+    auto result_node = next_node(node);
+    return (result_node ? &(result_node->key) : nullptr);
+}
+
+template<typename T>
+void BST<T>::insert(const value_type &key) {
+    Node *node = root;
+    Node *key_node = new Node{key};
+    while (node) {
+        key_node->parent = node;
+        if (node->key < key) {
+            node = node->right;
+        } else {
+            node = node->left;
+        }
+    }
+    if (key_node->parent) {
+        if (key_node->parent->key < key_node->key) {
+            key_node->parent->right = key_node;
+        } else {
+            key_node->parent->left = key_node;
+        }
+    } else {
+        this->root = key_node;
+    }
+    ++this->_size;
+}
+
+template<typename T>
+void BST<T>::erase(const value_type &key) {
+    Node *node = find_node(root, key);
+    erase_node(node);
+}
+
+template<typename T>
+void BST<T>::get_ordered(std::vector<value_type> &data) const {
+    data.clear();
+    data.reserve(_size);
+    get_ordered(data, root);
+}
+
+template<typename T>
+typename BST<T>::Node *BST<T>::find_node(BST::Node *curr_node, const value_type &key) const {
+    if (!curr_node) {
+        return nullptr;
+    }
+    if (curr_node->key > key) {
+        return find_node(curr_node->left, key);
+    }
+    if (curr_node->key < key) {
+        return find_node(curr_node->right, key);
+    }
+    return curr_node;
+}
+
+template<typename T>
+typename BST<T>::Node *BST<T>::next_node(const BST::Node *node) const {
+    Node *curr_node = find_node(root, node->key);
+    if (curr_node->right) {
+        curr_node = curr_node->right;
+        while (curr_node->left) {
+            curr_node = curr_node->left;
+        }
+        return curr_node;
+    }
+    while (curr_node->parent && curr_node->parent->right == curr_node) {
+        curr_node = curr_node->parent;
+    }
+    return (curr_node->parent ? curr_node->parent : nullptr);
+}
+
+template<typename T>
+void BST<T>::get_ordered(std::vector<value_type> &data, BST::Node *node) const {
+    if (node) {
+        get_ordered(data, node->left);
+        data.push_back(node->key);
+        get_ordered(data, node->right);
+    }
+}
+
+template<typename T>
+void BST<T>::erase_node(BST::Node *node) {
+    if (!node || !root) {
+        return;
+    }
+
+    if (node == root) {
+        if (!node->left && !node->right) {
+            delete node;
+            root = nullptr;
+            --_size;
+            return;
+        }
+
+        if (node->left && !node->right) {
+            root->key = root->left->key;
+            erase_node(root->left);
+            return;
+        }
+
+        if (!node->left && node->right) {
+            root->key = root->right->key;
+            erase_node(root->right);
+            return;
+        }
+
+        Node *node_next = next_node(root);
+        root->key = node_next->key;
+        erase_node(node_next);
+        return;
+    }
+
+    if (!node->left && !node->right) { // no children
+        (node->parent->left == node ? node->parent->left : node->parent->right) = nullptr;
+        delete node;
+        --_size;
+        return;
+    }
+
+    if (node->right) { // only right child
+        (node->parent->left == node ? node->parent->left : node->parent->right) = node->right;
+        node->right->parent = node->parent;
+        delete node;
+        --_size;
+        return;
+    }
+
+    if (node->left) { // only left child
+        (node->parent->left == node ? node->parent->left : node->parent->right) = node->left;
+        node->left->parent = node->parent;
+        delete node;
+        --_size;
+        return;
+    }
+
+    Node *node_next = next_node(node);
+    if (node_next) {
+        node->key = node_next->key;
+        erase_node(node_next);
+    }
+}
+
+template<typename T>
+void BST<T>::erase_subtree(BST::Node *node) {
+    if (node) {
+        erase_subtree(node->left);
+        erase_subtree(node->right);
+        erase_node(node);
+    }
+}
 
 
 int main() {
